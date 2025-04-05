c5829e7c5ae9fe0b69c930e58792d12f1a8ffce4
Nihar Mondal <developernihar@gmail.com>
2025-04-05 23:13:25 +0600

    customised: lecture model

--- src/modules/lecture/lecture.interface.ts
+++ src/modules/lecture/lecture.interface.ts
@@ -1,11 +1,16 @@
 import { Types } from "mongoose";
 
+type TAttachment = {
+	url: string;
+	fileType: "video" | "text" | "pdf";
+};
 export interface ILecture {
 	title: string;
 	slug: string;
-	videoUrl: string;
+	content: string;
 	type: string;
+	duration: string;
 	module: Types.ObjectId;
-	attachments: string;
+	attachments: TAttachment[];
 	isDeleted: boolean;
 }
